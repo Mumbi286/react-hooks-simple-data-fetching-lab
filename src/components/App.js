037f79e623bebe2ef28c03f57d4ebc0f8fd78476
Christine Mumbi <christinemumbi286@gmail.com>
2025-10-30 14:02:25 +0300

    corrected the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      {dogImage && <img src={dogImage} alt="A Random Dog" />}
+    </div>
+  );
+}
+
+export default App;
+
